1846e61c575cc2edd34a646db74ea639f04e812e
Bekah <beclau@byui.edu>
2020-09-25 14:56:55 -0600

    updated site

---
+++ web/home/assignment.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bekah Claussen | CSE 341 Assignments</title>
+    <meta name="description"
+        content="Bekah Claussen CSE 341 assignments">
+
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Quicksand">
+
+
+</head>
+
+<body>
+    <main>
+    <h1>BEKAH CLAUSSEN</h1>
+    <p>Assignments pending</p>
+    </main>
+
+</body>
+
+</html>
--- web/home/index.php
+++ web/home/index.php
@@ -5,27 +5,36 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bekah Claussen | CSE 341</title>
-    <meta name="description"
-        content="Bekah Claussen CSE 341 course content">
+    <meta name="description" content="Bekah Claussen CSE 341 course content">
 
 
     <link rel="stylesheet" href="css/style.css">
 
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Quicksand">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand">
 
 
 </head>
 
 <body>
     <main>
-        <div class="logos">
-        <figure>
-            <a href="assignment.php" ><img src="images/assignment.png" class="logoLink" alt="web logo"></a>
-        </figure>
-    </div>
-    <h1>BEKAH CLAUSSEN</h1>
-    <p></p>
+        <div class="logo">
+            <figure>
+                <a href="assignment.php"><img src="images/assignment.png" class="logoLink" alt="web logo"></a>
+            </figure>
+        </div>
+        <div class="text">
+        <h1>BEKAH CLAUSSEN</h1>
+        <p>I'm Bekah Claussen. Since I can talk about anything I want and I don't want to discuss personal information,
+            I will say that right now my biggest passion is preventing Trump from being reelected. He is a liar, and a
+            cheat,
+            and every day he violates the Constitution <a
+                href="https://www.theusconstitution.org/blog/president-trump-mocking-the-constitution/"
+                target="_blank">(this is not exaggeration)</a>. He is a very real threat to our democracy. Not electing
+            him goes beyond partisan politics. We can vote out Biden in four years if we want, but at least we will
+            still have a democracy. <a href="https://secure.anedot.com/the-lincoln-project/donate" target="_blank">Go
+                here</a> to donate to prevent Trump's reelection.
+        </p>
+        </div>
     </main>
 
 </body>
