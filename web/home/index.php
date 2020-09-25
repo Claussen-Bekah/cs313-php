e69027e79c3c79f15bd2a118cc22cf42b98d98e0
Bekah <beclau@byui.edu>
2020-09-25 14:30:12 -0600

    new files

---
+++ web/home/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bekah Claussen | CSE 341</title>
+    <meta name="description"
+        content="Bekah Claussen CSE 341 course content">
+
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Quicksand">
+
+
+</head>
+
+<body>
+    <main>
+        <div class="logos">
+        <figure>
+            <a href="assignment.php" ><img src="images/assignment.png" class="logoLink" alt="web logo"></a>
+        </figure>
+    </div>
+    <h1>BEKAH CLAUSSEN</h1>
+    <p></p>
+    </main>
+
+</body>
+
+</html>
