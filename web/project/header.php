fbb13fef8a1dca10d4264344908abc9643f2fa5f
Bekah <beclau@byui.edu>
2020-10-15 15:36:15 -0600

    css

--- web/project/header.php
+++ web/project/header.php
@@ -1,6 +1,6 @@
 
 <nav>
-<a href="index.php"><img src="images/logo.png" alt="cache logo"></a>
+<a href="index.php"><img class="navImg" src="images/logo.png" alt="cache logo"></a>
 <ul>
 <li><a href="items.php">Storage Items</a></li>
 <li><a href="list.php">Grocery Lists</a></li>
