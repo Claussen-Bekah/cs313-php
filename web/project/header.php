d4d9d9a1da811cfb4b1ca5e3e9895c69c9875481
Bekah <beclau@byui.edu>
2020-10-16 22:41:03 -0600

    modified pages

--- web/project/header.php
+++ web/project/header.php
@@ -2,7 +2,7 @@
 <nav>
 <a href="index.php"><img class="navImg" src="images/logo.png" alt="cache logo"></a>
 <ul>
-<li><a href="items.php">Storage Items <span class="arrow">></span></a></li>
-<li><a href="list.php">Grocery Lists <span class="arrow">></span></a></li>
+<li><a href="items.php" class="navLink">Storage Items <span class="arrow">></span></a></li>
+<li><a href="list.php" class="navLink">Grocery Lists <span class="arrow">></span></a></li>
 </ul>
 </nav>
