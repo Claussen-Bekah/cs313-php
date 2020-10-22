a63a1a55e50d1bce215e5a518d0ca8788d67bedb
Bekah <beclau@byui.edu>
2020-10-22 12:40:48 -0600

    added

--- web/project/newlist.php
+++ web/project/newlist.php
@@ -1,46 +1,46 @@
 <?php
 
-function dataConnect() {
+// function dataConnect() {
 
-    $db = NULL;
+//     $db = NULL;
 
-    try
-        {
-        $dbUrl = getenv('DATABASE_URL');
+//     try
+//         {
+//         $dbUrl = getenv('DATABASE_URL');
 
-        $dbOpts = parse_url($dbUrl);
+//         $dbOpts = parse_url($dbUrl);
 
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"],'/');
+//         $dbHost = $dbOpts["host"];
+//         $dbPort = $dbOpts["port"];
+//         $dbUser = $dbOpts["user"];
+//         $dbPassword = $dbOpts["pass"];
+//         $dbName = ltrim($dbOpts["path"],'/');
 
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+//         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-    catch (PDOException $ex)
-        {
-        echo 'Error!: ' . $ex->getMessage();
-    die();
-    }
+//         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//         }
+//     catch (PDOException $ex)
+//         {
+//         echo 'Error!: ' . $ex->getMessage();
+//     die();
+//     }
 
-    return $db;
-}
+//     return $db;
+// }
  
 
-$itemList = "<select name='itemId' id='itemId'><option disabled selected value> -- select an option -- </option>";
-    foreach ($db->query('SELECT id, item_description FROM item ORDER BY item_description') as $item){
+// $itemList = "<select name='itemId' id='itemId'><option disabled selected value> -- select an option -- </option>";
+//     foreach ($db->query('SELECT id, item_description FROM item ORDER BY item_description') as $item){
 
-        $itemId = $item['id'];
-        $itemName = $item['item_description'];
+//         $itemId = $item['id'];
+//         $itemName = $item['item_description'];
 
-        $itemList .= "<option value='$itemId'";
+//         $itemList .= "<option value='$itemId'";
         
-        $itemList.= ">$itemName</option>";
-    }
-$itemList .= '</select>';
+//         $itemList.= ">$itemName</option>";
+//     }
+// $itemList .= '</select>';
 
 
 // $listName = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
