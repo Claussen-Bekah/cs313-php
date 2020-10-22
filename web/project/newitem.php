1916e740fc6c4bf03dfcfc6de1e07b122b6fd4cd
Bekah <beclau@byui.edu>
2020-10-21 21:14:46 -0600

    modified pages

--- web/project/newitem.php
+++ web/project/newitem.php
@@ -63,17 +63,22 @@ function newItem($itemName, $itemNumber, $unitId, $categoryId) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cache | Food Storage Management</title>
+
+    <link rel="icon" href="images/favicon.png" type="image" sizes="16x16">
 
     <link rel="stylesheet" href="styles/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
+<header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/project/header.php'; ?>
+    </header>
     <?php
     if(isset($_POST['submitItem'])) {
         newItem($itemName, $itemNumber, $unitId, $categoryId);
     } ?>
-    <h1><?php echo $itemName?> successfully added</h1>
+    <h4><?php echo $itemName?> successfully added</h4>
     <a href="items.php" class="backBtn"><-- Go Back</a>
 </body>
 </html>
