b62185e5ca07623e1531c3feba1414bf2bf75731
Bekah <beclau@byui.edu>
2020-10-23 21:44:16 -0600

    added

--- web/project/items.php
+++ web/project/items.php
@@ -148,7 +148,7 @@ include("functions.php");
 
             deleteItem($itemId);
 
-            header('location: items.php#')
+            header('location: items.php#');
              
         }
 
